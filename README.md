4e82286d71250ce8554159639420374971da2815
François Roget <francoisroget@Francoiss-iMac.local>
2020-08-14 20:08:02 +0200

    Add project name replacement

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# starter-kit
+# %PROJECT_NAME%
 Example of starter-kit repository that auto-unlink itself from git
--- setup-project.sh
+++ setup-project.sh
@@ -5,5 +5,8 @@ echo 'Checking project structure...'
 echo 'Removing .git structure'
 rm -rf .git
 
+echo 'Updating project name'
+grep -rl %PROJECT_NAME% . | xargs sed -i '' 's/%PROJECT_NAME%/My\ Project/g'
+
 echo 'cleaning up...'
 rm -f setup-project.sh
